e59560560fb5f8f8f1f4842c821458d06868bfad
Sohaib Nisar <sohaib39826@gmail.com>
2020-05-31 14:24:25 +0500

    Sohaib Nisar

---
+++ app-functions/functions/util/admin.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./socialmedia-76e8b-firebase-adminsdk-3hprt-332f08b52d.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialmedia-76e8b.firebaseio.com",
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
