bf1ca70131ff58d958ac28af5030f487ab4536a2
Sohaib Nisar <sohaib39826@gmail.com>
2020-05-31 20:01:09 +0500

    Sohaib Nisar

--- app-functions/functions/util/admin.js
+++ app-functions/functions/util/admin.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../socialmedia-76e8b-firebase-adminsdk-3hprt-332f08b52d.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://socialmedia-76e8b.firebaseio.com",
+  
 });
 
 const db = admin.firestore();
--- app-functions/functions/util/firebaseConfig.js
+++ app-functions/functions/util/firebaseConfig.js
@@ -1,12 +1,6 @@
 const firebase = require("firebase");
 const firebaseConfig = {
-  // apiKey: "AIzaSyA0GXe6hxCvBxVz3V4x0X6aZhs9DyXcJn8",
-  // authDomain: "socialmedia-76e8b.firebaseapp.com",
-  // databaseURL: "https://socialmedia-76e8b.firebaseio.com",
-  // projectId: "socialmedia-76e8b",
-  // storageBucket: "socialmedia-76e8b.appspot.com",
-  // messagingSenderId: "1039962419648",
-  // appId: "1:1039962419648:web:8f54cc4adca068ac89d825",
+
 };
 firebase.initializeApp(firebaseConfig);
 exports.firebase = firebase;
